963439f47f79ec90925b99410870de44ac70a1b9
Guo Yang <94389344+gy261@users.noreply.github.com>
2022-09-07 17:15:21 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,20 @@
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
+
+import math
+
+import utils
+
+# Embedding
+# Positional Encoding
+# MASK
+# Multihead-Attention
+# FFN
+
+class FFN(nn.Module):
+    def __init__(self,):
+    
+    def forward():
+        
+    def 
